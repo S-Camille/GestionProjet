9ed60eed771714587cda119de5dc9d5ea34b6d88
ThibaultWIRTZ <wirtzthibault@gmail.com>
2018-12-21 18:23:05 +0100

    vue & route choix du type de personne

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/MarchésPublicsFaciles.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
@@ -44,7 +46,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ url('type_selection') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/type_selection', 'type_selection');
