83c3fc92e105bfeeaa9db0b7f369543bb98e2072
JesterHeads <benjamin.thouvenin.54220@gmail.com>
2019-01-13 17:09:51 +0100

    fix migration structure

--- database/migrations/2018_12_21_152855_create_structure_table.php
+++ database/migrations/2018_12_21_152855_create_structure_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEntrepriseTable extends Migration
+class CreateStructureTable extends Migration
 {
     /**
      * Run the migrations.
