5d19fc4323c6db8df2b094e0846fdc7a45bf66ea
a w <antoine.wittmann@hotmail.fr>
2019-01-13 13:37:10 +0100

    fixes and updates profile infos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,12 +31,10 @@ class HomeController extends Controller
 
             if($type_personne=='soumissionnaire'){ 
                 $hasEntreprise = DB::table('entreprise')->select('id')->where('id_gerant',Auth::id())->pluck('id');
-                session(['no_structure' => empty($hasEntreprise]);
-              
+                session(['no_structure' => (sizeof($hasEntreprise)==0)]);
                 if(isset($hasEntreprise[0])){
-                    session(['no_structure' => false]);
                     $hasExComptable = DB::table('exercice_comptable')->select('id')->where('id_entreprise',$hasEntreprise[0])->pluck('id');
-                    session(['no_ex_comptable' => empty($hasExComptable]);
+                    session(['no_ex_comptable' => (sizeof($hasExComptable)==0)]);
                 }
             }
             
