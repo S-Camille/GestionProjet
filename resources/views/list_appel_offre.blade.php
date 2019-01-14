b33f2c8e2e0f7a3c8d6ac45ab235bf83729c8061
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2019-01-14 12:09:41 +0100

    Ajout du tri par date de début pour la liste des appels d'offreé

--- resources/views/list_appel_offre.blade.php
+++ resources/views/list_appel_offre.blade.php
@@ -8,12 +8,12 @@
                     <div class="panel-heading bigFont">Liste d'appel d'offre</div>
                     <div class="panel-body list">
                         <div style="background:lightblue;border-bottom:1px solid grey;" class="row">
-                            <div class="col-xs-3 col-md-3">Titre</div><div class="col-xs-6 col-md-7">Description</div><div class="col-md-2">Date de fin</div>                            
+                            <div class="col-xs-3 col-md-3">Titre</div><div class="col-xs-6 col-md-5">Description</div><div class="col-md-2">Date de début</div><div class="col-md-2">Date de fin</div>                            
                         </div>
                         @if(sizeof($appel_offre)!=0)
                             @for ($i = 0; $i < sizeof($appel_offre); $i++)
                                 <div class="row list-elt">
-                                    <div class="col-xs-3 col-md-3">{{$appel_offre[$i]->titre}}</div><div class="col-xs-6 col-sd-7 col-md-7">{{$appel_offre[$i]->description}}</div><div class="col-sd-2 col-md-2">{{$appel_offre[$i]->date_fin}}</div>
+                                    <div class="col-xs-3 col-md-3">{{$appel_offre[$i]->titre}}</div><div class="col-xs-6 col-sd-5 col-md-5">{{$appel_offre[$i]->description}}</div><div class="col-sd-2 col-md-2">{{$appel_offre[$i]->date_debut}}</div><div class="col-sd-2 col-md-2">{{$appel_offre[$i]->date_fin}}</div>
                                 </div>
                             @endfor
                         @else
